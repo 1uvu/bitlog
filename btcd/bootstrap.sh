4fc5e8d22796f04f18920aa4fbb0ee0bf9c61067
1uvu <zjh98@vip.qq.com>
2022-07-02 17:30:03 +0800

    update bootstrap

--- btcd/bootstrap.sh
+++ btcd/bootstrap.sh
@@ -1,25 +1,69 @@
 #!/bin/bash
 
-pwd=$PWD
-cd "$PWD"/docker-compose/ || exit 0
-echo "copy env example to env"
-cp .env.example .env
-source .env
+temp_dir=./temp
+pwd=$PWD/docker-compose
 
-if [ -z "${BTCD_ROOT_DIR}" ]; then
-  echo "error: peer root dir not set"
-  exit 255
-fi
-if [ -d "${BTCD_ROOT_DIR}" ]; then
-  echo "error: peer root dir has existed"
-  exit 254
-fi
-mkdir -p "${BTCD_ROOT_DIR}"
-echo "copy peer example to peer ${CONTAINER_NAME}"
-cp peer-example/btcd.conf "${BTCD_ROOT_DIR}"/
+function precheck() {
+  if [ !-f "$pwd/tmpl/.env.tmpl" ]; then
+    exitWithError ".env tmpl not prepare"
+  fi
+  if [ !-f "$pwd/tmpl/btcd.conf.tmpl" ]; then
+    exitWithError "btcd.conf.tmpl not prepared"
+  fi
+  source "$pwd/tmpl/.env.tmpl"
+  if [ -z "${CONTAINER_NAME}" ]; then
+    exitWithError "peer name not set, please set"
+  fi
+  if [ -z "${BTCD_ROOT_DIR}" ]; then
+    exitWithError "btcd root dir not set, please set"
+  fi
+  if [ -d "${CONTAINER_NAME}" ]; then
+    exitWithError "peer container dir has existed, please rm or rename peer"
+  fi
+}
 
-echo "bootstrap peer"
-docker-compose -f docker-compose-tmpl.yaml up -d
+function prepare() {
+  cd "$pwd"/ || exit 0
+  mkdir "${temp_dir}"
+}
 
-echo "bootstrap success"
-cd "$pwd" || exit 0
+function exitWithError() {
+  errorMsg=$1
+  if [ !-z "${errorMsg}" ]; then
+    echo "error: ${errorMsg}"
+  fi
+  if [ -d "${temp_dir}" ]; then
+    echo "clean temp files..."
+    sudo rm -rf "${pwd}"/"${temp_dir}"/
+  fi
+  exit 0
+}
+
+function main() {
+  echo "copy .env tmpl"
+  cp "$pwd"/tmpl/.env.tmpl "${temp_dir}"/.env
+  echo "copy btcd.conf tmpl"
+  mkdir "$pwd"/"${temp_dir}/${BTCD_ROOT_DIR}"
+  cp "$pwd"/tmpl/btcd.conf.tmpl "$pwd"/"${temp_dir}"/${BTCD_ROOT_DIR}"/btcd.conf
+
+  echo "copy tmpl to peer ${CONTAINER_NAME}"
+  mkdir -p "$pwd"/"${CONTAINER_NAME}"/
+  cp -r "$pwd"/"${temp_dir}/* "$pwd"/"${CONTAINER_NAME}"/
+  cp docker-compose-tmpl.yaml "$pwd"/"${CONTAINER_NAME}"/
+
+  echo "source env"
+  cd "$pwd"/"${CONTAINER_NAME}"
+  source .env
+
+  echo "bootstrap peer"
+  docker-compose up -d
+
+  echo "bootstrap success"
+  exitWithError
+}
+
+precheck
+
+prepare
+
+main
