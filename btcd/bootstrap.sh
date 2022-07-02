d3f1b243f127665c1ed0c23743a8cb4d2f08f121
1uvu <zjh98@vip.qq.com>
2022-07-02 18:41:35 +0800

    update bootstrap sh
    Change-Id: I6a5d1a17ce7d77aa4cb77389cfb51489a9fd81ad

--- btcd/bootstrap.sh
+++ btcd/bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+subnet_name=bitlog_peer_network
 temp_dir=temp
 pwd=${PWD}/docker-compose
 
@@ -17,7 +18,7 @@ function precheck() {
   if [ -z "${BTCD_ROOT_DIR}" ]; then
     exitWithError "btcd root dir not set, please set"
   fi
-  if [ -d "${pwd}/${CONTAINER_NAME}" ]; then
+  if [ -d "${pwd}/peers/${CONTAINER_NAME}" ]; then
     exitWithError "peer container dir has existed, please rm or rename peer"
   fi
 }
@@ -34,23 +35,34 @@ function exitWithError() {
     echo "error: ${errorMsg}"
   fi
   if [ -d "${temp_dir}" ]; then
-    echo "clean temp files..."
+    echo "clean temp files"
     sudo rm -rf "${pwd}"/"${temp_dir}"/
   fi
   exit 0
 }
 
+function bootstrap() {
+  docker network create --driver=bridge --subnet="${SUBNET_CIDR}" "${subnet_name}"
+  docker-compose up -d
+}
+
 function main() {
+  echo "precheck process"
+  precheck
+
+  echo "prepare process"
+  prepare
+
   echo "copy .env tmpl"
   cp "${pwd}/tmpl/.env.tmpl" "${temp_dir}/.env"
   echo "copy btcd.conf tmpl"
-  mkdir "${pwd}/${temp_dir}/${BTCD_ROOT_DIR}"
-  cp "${pwd}/tmpl/btcd.conf.tmpl" "${pwd}/${temp_dir}/${BTCD_ROOT_DIR}/btcd.conf"
+  cp "${pwd}/tmpl/btcd.conf.tmpl" "${pwd}/${temp_dir}/btcd.conf"
 
   echo "copy tmpl to peer ${CONTAINER_NAME}"
   mkdir -p "${pwd}/peers/${CONTAINER_NAME}/"
   cp "${pwd}/${temp_dir}/.env" "${pwd}/peers/${CONTAINER_NAME}/"
-  cp -r "${pwd}/${temp_dir}/${BTCD_ROOT_DIR}" "${pwd}/peers/${CONTAINER_NAME}/"
+  mkdir "${pwd}/peers/${CONTAINER_NAME}/${BTCD_ROOT_DIR}/"
+  cp "${pwd}/${temp_dir}/btcd.conf" "${pwd}/peers/${CONTAINER_NAME}/${BTCD_ROOT_DIR}/"
   cp "${pwd}/docker-compose.yaml.tmpl" "${pwd}/peers/${CONTAINER_NAME}/docker-compose.yaml"
 
   echo "source env"
@@ -58,15 +70,11 @@ function main() {
   source .env
 
   echo "bootstrap peer"
-  docker-compose up -d
+  bootstrap
 
   rm -rf "${pwd}/${temp_dir}"
   echo "bootstrap success"
   exitWithError
 }
-
-precheck
-
-prepare
 
 main
